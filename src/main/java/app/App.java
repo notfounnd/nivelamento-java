da4501c8411794e25024bfb884cf9c822d9b9658
Júnior Sbrissa <jr.sbrissa@hotmail.com>
2023-05-25 20:31:18 -0300

    m03a04_classes

---
+++ src/main/java/app/App.java
@@ -0,0 +1,10 @@
+package app;
+
+import dto.Viagem;
+
+public class App {
+    public static void main(String[] args) {
+        Viagem viagem = new Viagem();
+
+    }
+}
---
+++ src/main/java/dto/Viagem.java
@@ -0,0 +1,7 @@
+package dto;
+
+public class Viagem {
+    // Isso é um comentário que está dentro da classe
+}
+
+// Isso é um comentário que está fora da classe
